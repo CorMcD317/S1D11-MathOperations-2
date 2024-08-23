9417326f37b3d8ebf8b27b79d5539988f719e3d6
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-08-23 08:04:29 -0400

    Initial commit

---
+++ Math2.py
@@ -0,0 +1,8 @@
+miles = 26
+minutes = 294
+hours = minutes/60
+mph = miles/hours
+print("How many miles did you run?", miles)
+print("How many minutes did it take you?", minutes)
+print("How many hours did it take you?", hours)
+print("Speed in mph:", round(mph,3))
